669086ffdc01b0a07a6aaf258fad572e89167460
boobalan <boobalan.shanmugasundaram@infibeam.net>
2018-02-13 16:47:08 +0530

    users alignment

--- twitter/app/controllers/users_controller.rb
+++ twitter/app/controllers/users_controller.rb
@@ -14,8 +14,8 @@ class UsersController < ApplicationController
   def status_set
         @user = User.find(params[:user_id])
         @user.update_attributes(:designation => params["format"])
-	@query = "$(\"##{params[:user_id]}\").text(\"#{params[:format]}\")"
-	render js: @query
+        @query = "$(\"##{params[:user_id]}\").text(\"#{params[:format]}\")"
+	      render js: @query
   end
 
 
@@ -36,139 +36,124 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-
     if( User.find_by username: params["user"]["username"])
-	respond_to do |format|
-	  format.html {redirect_to new_user_url, notice: 'Username already exists'}
-	end
+	     respond_to do |format|
+         format.html {redirect_to new_user_url, notice: 'Username already exists'}
+	      end
     elsif(!(params["user"]["password"] .eql? params["user"]["confirm_password"]))
-	respond_to do |format|
-	  format.html {redirect_to new_user_url, notice: 'Passwords doesnot match'}
-	end
+	     respond_to do |format|
+	        format.html {redirect_to new_user_url, notice: 'Passwords doesnot match'}
+	       end
     elsif(User.find_by email: params["user"]["email"])
-	respond_to do |format|
-	  format.html {redirect_to new_user_url, notice: 'Email Id already exists'}
-	end
+	     respond_to do |format|
+	        format.html {redirect_to new_user_url, notice: 'Email Id already exists'}
+	       end
     else
-    @user = User.new(user_params)
-    @user.designation="user"
-    respond_to do |format|
-      if @user.save
-	session[:username] = params["user"]["username"]
-	puts "aaaaaaaaaaaaaaaaaaaaaa"
-	puts session[:username]
-        format.html { redirect_to users_url, notice: 'User was successfully created.' }
-        #format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+      @user = User.new(user_params)
+      @user.designation="user"
+      respond_to do |format|
+        if @user.save
+	         session[:username] = params["user"]["username"]
+           puts "aaaaaaaaaaaaaaaaaaaaaa"
+	         puts session[:username]
+           format.html { redirect_to users_url, notice: 'User was successfully created.' }
+           #format.json { render :show, status: :created, location: @user }
+         else
+           format.html { render :new }
+           format.json { render json: @user.errors, status: :unprocessable_entity }
+         end
       end
     end
+  end
+
+   # PATCH/PUT /users/1
+   # PATCH/PUT /users/1.json
+   def update
+     respond_to do |format|
+       if @user.update(user_params)
+         format.html { redirect_to @user, notice: 'User was successfully updated.' }
+         format.json { render :show, status: :ok, location: @user }
+       else
+         format.html { render :edit }
+         format.json { render json: @user.errors, status: :unprocessable_entity }
+       end
+     end
    end
-  end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+   # DELETE /users/1
+   # DELETE /users/1.json
+   def destroy
+     @user.destroy
+     respond_to do |format|
+       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+       format.json { head :no_content }
+     end
+   end
+
+   def dpchange
+     @usr1 = User.where("username like '" + session[:username] + "'")[0]
+     @usr1.update_attributes(:dp => params[:newdp])
+     puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaassssssssss"
+     puts params[:newdp]
+   end
+
+   def passwordChange
+     @usr1 = User.where("username like '" + session[:username] + "'")[0]
+     @usr1.update_attributes(:password => params[:password])
+     render js:""
+   end
+
+   def getDp
+     @usr1 = User.where("username like '" + session[:username] + "'")[0]
+     render js: @usr1.dp.thumb.url
+   end
+
+   def login
+     if( User.find_by username: params[:uname])
+    	  @newuser = User.find_by username: params[:uname]
+        if(@newuser[:password] .eql? params[:password])
+          respond_to do |format|
+            if(@newuser[:designation]) .eql? "moderator"
+              session[:username] = params[:uname]
+              puts @newuser[:designation]
+              format.html { redirect_to new_tweet_url}
+            elsif(@newuser[:designation]) .eql? "admin"
+              session[:username] = params[:uname]
+			        puts @newuser[:designation]
+			        format.html { redirect_to type_change_url}
+            else
+              format.html { redirect_to new_tweet_url, flash[:notice] => "LoggedIn Successfully" }
+		   	      session[:username] = params[:uname]
+            end
+          end
+        else
+          respond_to do |format|
+            format.html {redirect_to users_url, notice: 'Invalid password'}
+          end
+        end
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        respond_to do |format|
+    			format.html {redirect_to users_url, notice: 'Invalid username'}
+        end
       end
     end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  def dpchange
-          @usr1 = User.where("username like '" + session[:username] + "'")[0]
-          @usr1.update_attributes(:dp => params[:newdp])
-          puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaassssssssss"
-          puts params[:newdp]
-
-  end
-
-  def passwordChange
-          @usr1 = User.where("username like '" + session[:username] + "'")[0]
-          @usr1.update_attributes(:password => params[:password])
-          render js:""
-  end
-
-  def getDp
-    @usr1 = User.where("username like '" + session[:username] + "'")[0]
-    render js: @usr1.dp.thumb.url
-  end
-
-  def login
-    if( User.find_by username: params[:uname])
-    	@newuser = User.find_by username: params[:uname]
-        if(@newuser[:password] .eql? params[:password])
-		respond_to do |format|
-			if(@newuser[:designation]) .eql? "moderator"
-				session[:username] = params[:uname]
-			  	puts @newuser[:designation]
-			  	format.html { redirect_to new_tweet_url}
-			elsif(@newuser[:designation]) .eql? "admin"
-			      session[:username] = params[:uname]
-			      puts @newuser[:designation]
-			      format.html { redirect_to type_change_url}
-	       		else
-	       	       	      format.html { redirect_to new_tweet_url, flash[:notice] => "LoggedIn Successfully" }
-		   	      session[:username] = params[:uname]
-			end
-        	end
-    	else
-		respond_to do |format|
-			format.html {redirect_to users_url, notice: 'Invalid password'}
-		end
-  	end
-    else
-    		respond_to do |format|
-    			format.html {redirect_to users_url, notice: 'Invalid username'}
-		end
-    end
-  end
-  def check_user
-
-
+    def check_user
       @checkuser = User.find_by username: params[:username]
       if @checkuser .nil?
-
-	render js: ""
-
+        render js: ""
       elsif(@checkuser[:username] .eql? params[:username])
-	render js: "username already exist"
+        render js: "username already exist"
       end
-
-  end
-
-  def checkmail_user
-
-
+    end
+    def checkmail_user
       @checkuser = User.find_by email: params[:email]
       if @checkuser .nil?
-
-	render js: " "
-
+        render js: " "
       elsif(@checkuser[:email] .eql? params[:email])
-	render js: "email Id already exist"
+        render js: "email Id already exist"
       end
-
-  end
-
-
-  private
+    end
+    private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
--- twitter/config/routes.rb
+++ twitter/config/routes.rb
@@ -1,31 +1,22 @@
 Rails.application.routes.draw do
+  get "/mod_front" => "tweets#mod_front"
+  post "/users/dpchange" => "users#dpchange"
+  get "users/getDp" => "users#getDp"
+  get "users/changepassword" =>"users#passwordChange"
+  get "/filter_tweets" => "tweets#filter_tweets"
+  resources :tweets do
+    get 'set_stat' => 'tweets#set_stat'
+    get "indexn"
+  end
 
-
-get "/mod_front" => "tweets#mod_front"
-post "/users/dpchange" => "users#dpchange"
-get "users/getDp" => "users#getDp"
-get "users/changepassword" =>"users#passwordChange"
-get "/filter_tweets" => "tweets#filter_tweets"
-
-
-resources :tweets do
-  get 'set_stat' => 'tweets#set_stat'
-  get "indexn"
-end
-get "/moderator" => 'tweets#moderator'
+  get "/moderator" => 'tweets#moderator'
   resources :users do
     get "/status_set" => 'users#status_set'
   end
 
-
-  #resources :users
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/type_change" => 'users#type_change'
   get "/stats" => "tweets#statistics"
   post 'users/login'
   get "/check_user" => "users#check_user"
   get "/checkmail_user" => "users#checkmail_user"
-
-
 end
